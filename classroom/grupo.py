e29f2bec237e4c3b7739597bea9ba06ed93d4dc3
yeehll02 <116982357+yeehll02@users.noreply.github.com>
2023-03-04 17:17:00 -0500

    taller4

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,25 +3,20 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
     def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
-            if self._asignaturas==None:
-                self._asignaturas=[]
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=None):
         if lista==None:
             lista=[]
         lista.append(alumno)
-        if  self.listadoAlumnos==None:
-            self.listadoAlumnos=lista
-        else:
-            self.listadoAlumnos = self.listadoAlumnos + lista
+        self.listadoAlumnos = self.listadoAlumnos + lista
         return lista
 
     def __str__(self):
